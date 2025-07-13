8aea84f755488211da71bf5dd41fdb2809c29775
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-13 16:58:33 +0600

    contact typo - discus to discuss

--- app/sections/Contact.jsx
+++ app/sections/Contact.jsx
@@ -6,7 +6,7 @@ import gsap from "gsap";
 
 const Contact = () => {
   const text = `Got a question, how or project Idea?
-    I’D love to hear from you and discus further!`;
+    I’D love to hear from you and discuss further!`;
   const items = [
     "imagine, I build",
     "imagine, I build",
