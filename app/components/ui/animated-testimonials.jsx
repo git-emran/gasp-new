3e387380977ed5fe3acddedf1cd12806ce068867
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-25 19:02:22 +0600

    testimonials dark mode fixed

--- app/components/ui/animated-testimonials.jsx
+++ app/components/ui/animated-testimonials.jsx
@@ -99,13 +99,13 @@ export const AnimatedTestimonials = ({ testimonials, autoplay = false }) => {
               ease: "easeInOut",
             }}
           >
-            <h3 className="text-2xl font-bold text-black dark:text-white">
+            <h3 className="text-2xl font-bold text-black">
               {testimonials[active].name}
             </h3>
             <p className="text-sm text-gray-500 dark:text-neutral-500">
               {testimonials[active].designation}
             </p>
-            <motion.p className="mt-8 text-lg text-gray-500 dark:text-neutral-300">
+            <motion.p className="mt-8 text-lg text-gray-500">
               {testimonials[active].quote.split(" ").map((word, index) => (
                 <motion.span
                   key={index}
