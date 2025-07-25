5d4a41f4b974fd03563501eb8c291dd0b7ed7efe
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-07-25 19:44:57 +0600

    New HeroText component added with container-text-flip

---
+++ app/components/HeroText.jsx
@@ -0,0 +1,9 @@
+import { ContainerTextFlip } from "./ui/container-text-flip";
+
+export function HeroText() {
+  return (
+    <ContainerTextFlip
+      words={["Designer", "Programmer", "Artist", "Father"]}
+    />
+  );
+}
--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -1,10 +1,14 @@
 import { useMediaQuery } from "react-responsive";
 import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { HeroText } from "../components/HeroText";
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
   const text = `Expert in Enterprise Design / Development`;
   return (
-    <section id="home" className="flex flex-col justify-end min-h-screen">
+    <section id="home" className="flex flex-col justify-between min-h-screen">
+      <div className="flex mt-[10rem] justify-center">
+        <HeroText />
+      </div>
       <AnimatedHeaderSection
         subTitle={"Designer • Developer"}
         title={"Emran Hossain"}
