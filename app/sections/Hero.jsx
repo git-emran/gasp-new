3299d83a634dfcf18d32fa00c8fc08be2ca93ca7
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-09-10 09:12:32 +0600

    update on what I am building

--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -3,30 +3,30 @@ import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
 import { HeroText } from "../components/HeroText";
 import { Link, Text } from "@radix-ui/themes";
 const Hero = () => {
-    const isMobile = useMediaQuery({ maxWidth: 853 });
-    const text = `Expert in Enterprise Design / Development`;
-    return (
-        <section id="home" className="flex flex-col justify-end min-h-screen">
-            <div className="px-10 justify-start">
+  const isMobile = useMediaQuery({ maxWidth: 853 });
+  const text = `Expert in Enterprise Design / Development`;
+  return (
+    <section id="home" className="flex flex-col justify-end min-h-screen">
+      <div className="px-10 justify-start">
 
-                <HeroText />
-            </div>
+        <HeroText />
+      </div>
 
-            <div className="px-10 pt-6 justify-start">
-                <Text as="div" className="text-gray-600">
-                    👨🏻‍💻 Building my own  <Link className="font-bold text-gray-700 underline" href="https://github.com/git-emran/simple-notes">Markdown</Link>{" "}application
-                </Text>
+      <div className="px-10 pt-6 justify-start">
+        <Text as="div" className="text-gray-600">
+          👨🏻‍💻 Building my own  <Link className="font-bold text-gray-700 underline" href="https://github.com/git-emran/simple-notes">Markdown</Link>{" "}Text Editor that uses Vim motion
+        </Text>
 
-            </div>
-            <AnimatedHeaderSection
-                subTitle={"Designer • Developer"}
-                title={"Emran Hossain"}
-                text={text}
-                textColor={"text-black"}
-            />
+      </div>
+      <AnimatedHeaderSection
+        subTitle={"Designer • Developer"}
+        title={"Emran Hossain"}
+        text={text}
+        textColor={"text-black"}
+      />
 
-        </section>
-    );
+    </section>
+  );
 };
 
 export default Hero;
