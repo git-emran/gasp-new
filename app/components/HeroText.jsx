85bf3156aeb1dd56efdb641dfe4c2159fd1ba77e
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-09-21 08:32:28 +0600

    removed the word father from the portfolio

--- app/components/HeroText.jsx
+++ app/components/HeroText.jsx
@@ -3,7 +3,7 @@ import { ContainerTextFlip } from "./ui/container-text-flip";
 export function HeroText() {
   return (
     <ContainerTextFlip
-      words={["Designer", "Programmer", "Artist", "Father"]}
+      words={["Designer", "Programmer", "Artist"]}
     />
   );
 }
