817eda9029d485ea8e19ddc72543a8ed81b55468
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-11-25 16:33:01 +0600

    Fixing the navbar height

--- app/sections/Navbar.jsx
+++ app/sections/Navbar.jsx
@@ -104,7 +104,7 @@ const Navbar = () => {
       <div className={isReady ? "block" : "hidden"}>
         <nav
           ref={navRef}
-          className="fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2"
+          className="fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-10 gap-y-10 md:w-1/2 md:left-1/2"
         >
           <div className="flex flex-col text-5xl gap-y-2 md:text-6xl lg:text-8xl">
             {["home", "works", "skills", "about", "praises", "contact"].map(
