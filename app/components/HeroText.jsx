c5ebd5feaa113889efe0f79abfc57adcecd8077d
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-03-24 19:00:31 +0600

    Hero text change

--- app/components/HeroText.jsx
+++ app/components/HeroText.jsx
@@ -3,7 +3,7 @@ import { ContainerTextFlip } from "./ui/container-text-flip";
 export function HeroText() {
   return (
     <ContainerTextFlip
-      words={["Designer", "Programmer", "Artist"]}
+      words={["Designer", "Programmer",]}
     />
   );
 }
